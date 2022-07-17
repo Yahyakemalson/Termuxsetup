2a3ede358e9d97caf9282889ebb837abeacf1ac6
Yahyakemalson <103384233+Yahyakemalson@users.noreply.github.com>
2022-07-17 21:56:16 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python2
+
+import os
+
+os.system("pkg  update")
+os.system("pkg install git")
+os.system("pkg install python2")
+os.system("pkg install python")
+os.system("pkg install python3")
+os.system("pkg install php")
+os.system("pkg install php7")
+os.system("pkg install bash")
+os.system("pip install proxy")
+os.system("pip install wordlist")
+os.system("apt update")
+os.system("pip install wordlist")
+os.system("apt install nodejs")
+os.system("pkg install pip2")
+os.system("pkg install nmap")
+os.system("termux-setup-storage")
+os.system("pkg install vim")
+os.system("pip install wordlist")
+os.system("pkg install hydra")
+os.system("pkg install openssl")
+os.system("pkg install curl")
+os.system("pkg install nano")
+os.system("pkg install perl")
+os.system("pkg install wget")
+os.system("pip install blockcypher")
+os.system("pip install satoshi")
+os.system("pip install moneywagon")
+os.system("pip install urllib")
+print("""
+
+kurulum bilti amk daha ne istiyon
+
+""")
